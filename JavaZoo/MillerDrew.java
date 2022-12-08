74e3901fea9fd6e399ae92ebad7afc1e87ad3774
Drew Miller <drew.miller@mrjaffesclass.com>
2022-12-08 11:45:34 -0800

    Override

--- JavaZoo/MillerDrew.java
+++ JavaZoo/MillerDrew.java
@@ -9,96 +9,40 @@ public class MillerDrew extends Animal {
         this.legs = 10;
     }
 
-    /**
-     * Getter for color
-     */
+    @Override
     public String getColor() {
         return this.color;
     }
 
-    /** 
-     * Setter for color
-     * 
-     * @param color New color
-     */
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    /**
-     * Getter for legs
-     */
+    @Override
     public int getLegs() {
         return this.legs;
     }
 
-    /** 
-     * Setter for legs
-     * 
-     * @param legs New number of legs
-     */
-    public void setLegs(int legs) {
-        this.legs = legs;
-    }
 
-    /** 
-     * Getter for studentName
-     * 
-     * @return The student name
-     */
+    @Override
     public String getStudentName() {
         return this.studentName;
     }
 
-    /**
-     * What does the animal say?  Descendant classes should override this
-     * method or default speak will generate "Yaba-daba-doo"
-     */
+    @Override
     public String speak() {
         return "beya-beya";
     }
 
-    /**
-     * This will return a String that describes the animal's general disposition.
-     * Sample dispositions could be "mean" or "friendly".  Use different adjectives
-     * in your method. The default disposition is "meh"
-     * 
-     * @return Disposition
-     */
+    @Override
     public String getDisposition() {
         return "savage";
     }
 
-    /**
-     * This will return the type of animal (i.e. "Dog", "Cat", etc)
-     * 
-     * @return The type of the animal
-     */
+    @Override
     public String getType() {
         return "Winged Gecko";
     }
 
-    /**
-     * This will return the animal's name. What's the default name?
-     * 
-     * @return The name of the animal (i.e. "Fido", "Rover")
-     */
+    @Override
     public String getName() {
         return "Mr. Bellinger";
-    }
-
-    /**
-     * toString() is a method that all Java classes implement.  It's where you can 
-     * generate a string that describes the object.  You should not override this
-     * method unless you know what you're doing
-     */
-    @Override
-    public String toString() {
-        StringBuffer buffer = new StringBuffer("");
-        buffer.append("It is a "+this.getType()+"\n");
-        buffer.append("Its name is "+this.getName()+", it is "+this.getColor()+" with "+this.getLegs()+" legs\n");
-        buffer.append("Its disposition is "+this.getDisposition()+" and it says "+this.speak()+"\n");
-        return buffer.toString();
     }
 
 }
